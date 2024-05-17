a1f8be91e647f562b4b7a928aa60f1e15596378c
FMSans <mfsans@foxmail.com>
2024-05-17 17:27:13 +0800

    update asm

--- code_gen/asm.go
+++ code_gen/asm.go
@@ -43,7 +43,13 @@ func (this *CodeGenerator) visit(node parser.Node) string {
 	switch node.Type() {
 	case parser.AstTypeProgram.Name():
 		asm = this.visitProgram(node)
-	// 表达式
+	//数组
+	case parser.AstTypeArrayLiteral.Name():
+		asm = this.visitArrayLiteral(node)
+	// 一元表达式
+	case parser.AstTypeUnaryExpression.Name():
+		asm = this.visitUnaryExpression(node)
+	// 二元表达式
 	case parser.AstTypeBinaryExpression.Name():
 		asm = this.visitBinaryExpression(node)
 	// 赋值
@@ -90,114 +96,84 @@ func (this *CodeGenerator) visitBinaryExpression(node parser.Node) string {
 	if node.Type() != parser.AstTypeBinaryExpression.Name() {
 		return ""
 	}
-	// op
-	switch node.(parser.BinaryExpression).Operator {
-	case "+":
-		// left
-		left := node.(parser.BinaryExpression).Left
-		leftAsm := this.visit(left)
-
-		// right
-		right := node.(parser.BinaryExpression).Right
-		rightAsm := this.visit(right)
-		utils.LogInfo("visitBinaryExpression", node.(parser.BinaryExpression).Left, node.(parser.BinaryExpression).Right)
-
-		// 退出来寄存器
-		rightReg := this.Register.ReturnRegPop()
-		leftReg := this.Register.ReturnRegPop()
-		// 暂存结果寄存器
-		resultReg := this.Register.ReturnRegAlloc()
-
-		return leftAsm + rightAsm + "add2 " + leftReg + " " + rightReg + " " + resultReg + "\n"
-	case "-":
-		// left
-		left := node.(parser.BinaryExpression).Left
-		leftAsm := this.visit(left)
-
-		// right
-		right := node.(parser.BinaryExpression).Right
-		rightAsm := this.visit(right)
-		utils.LogInfo("visitBinaryExpression", node.(parser.BinaryExpression).Left, node.(parser.BinaryExpression).Right)
-
-		// 退出来寄存器
-		rightReg := this.Register.ReturnRegPop()
-		leftReg := this.Register.ReturnRegPop()
-		// 暂存结果寄存器
-		resultReg := this.Register.ReturnRegAlloc()
-
-		return leftAsm + rightAsm + "sub2 " + leftReg + " " + rightReg + " " + resultReg + "\n"
-	case "*":
-		// left
-		left := node.(parser.BinaryExpression).Left
-		leftAsm := this.visit(left)
-
-		// right
-		right := node.(parser.BinaryExpression).Right
-		rightAsm := this.visit(right)
-		utils.LogInfo("visitBinaryExpression", node.(parser.BinaryExpression).Left, node.(parser.BinaryExpression).Right)
-
-		// 退出来寄存器
-		rightReg := this.Register.ReturnRegPop()
-		leftReg := this.Register.ReturnRegPop()
-		// 暂存结果寄存器
-		resultReg := this.Register.ReturnRegAlloc()
-
-		return leftAsm + rightAsm + "mul2 " + leftReg + " " + rightReg + " " + resultReg + "\n"
-	case "/":
-		// left
-		left := node.(parser.BinaryExpression).Left
-		leftAsm := this.visit(left)
-
-		// right
-		right := node.(parser.BinaryExpression).Right
-		rightAsm := this.visit(right)
-		utils.LogInfo("visitBinaryExpression", node.(parser.BinaryExpression).Left, node.(parser.BinaryExpression).Right)
-
-		// 退出来寄存器
-		rightReg := this.Register.ReturnRegPop()
-		leftReg := this.Register.ReturnRegPop()
-		// 暂存结果寄存器
-		resultReg := this.Register.ReturnRegAlloc()
-
-		return leftAsm + rightAsm + "div2 " + leftReg + " " + rightReg + " " + resultReg + "\n"
-	case "and":
-		// left
-		left := node.(parser.BinaryExpression).Left
-		leftAsm := this.visit(left)
-
-		// right
-		right := node.(parser.BinaryExpression).Right
-		rightAsm := this.visit(right)
-		utils.LogInfo("visitBinaryExpression", node.(parser.BinaryExpression).Left, node.(parser.BinaryExpression).Right)
-
-		// 退出来寄存器
-		rightReg := this.Register.ReturnRegPop()
-		leftReg := this.Register.ReturnRegPop()
-		// 暂存结果寄存器
-		resultReg := this.Register.ReturnRegAlloc()
-
-		return leftAsm + rightAsm + "bool_and " + leftReg + " " + rightReg + " " + resultReg + "\n"
-	case "or":
-		// left
-		left := node.(parser.BinaryExpression).Left
-		leftAsm := this.visit(left)
-
-		// right
-		right := node.(parser.BinaryExpression).Right
-		rightAsm := this.visit(right)
-		utils.LogInfo("visitBinaryExpression", node.(parser.BinaryExpression).Left, node.(parser.BinaryExpression).Right)
-
-		// 退出来寄存器
-		rightReg := this.Register.ReturnRegPop()
-		leftReg := this.Register.ReturnRegPop()
-		// 暂存结果寄存器
-		resultReg := this.Register.ReturnRegAlloc()
-
-		return leftAsm + rightAsm + "bool_or " + leftReg + " " + rightReg + " " + resultReg + "\n"
-	default:
-		utils.LogError("visitBinaryExpression invalid operator", node.(parser.BinaryExpression).Operator)
+	instructionMap := map[string]string{
+		"+":   "add2",
+		"-":   "sub2",
+		"*":   "mul2",
+		"/":   "div2",
+		"and": "bool_and",
+		"or":  "bool_or",
+		">=":  "bool_lget",
+		"<=":  "bool_sget",
+		"==":  "bool_eq",
+		"!=":  "bool_neq",
+		">":   "bool_gt",
+		"<":   "bool_st",
+		"+=":  "num_add_eq",
+		"-=":  "num_sub_eq",
+		"*=":  "num_mul_eq",
+		"/=":  "num_div_eq",
 	}
-	return ""
+	ins, ok := instructionMap[node.(parser.BinaryExpression).Operator]
+	if !ok {
+		utils.LogError("invalid visitBinaryExpression", node.(parser.BinaryExpression).Operator)
+		return ""
+	}
+	// left
+	left := node.(parser.BinaryExpression).Left
+	leftAsm := this.visit(left)
+
+	// right
+	right := node.(parser.BinaryExpression).Right
+	rightAsm := this.visit(right)
+	utils.LogInfo("visitBinaryExpression", node.(parser.BinaryExpression).Left, node.(parser.BinaryExpression).Right)
+
+	// 退出来寄存器
+	rightReg := this.Register.ReturnRegPop()
+	leftReg := this.Register.ReturnRegPop()
+	// 暂存结果寄存器
+	resultReg := this.Register.ReturnRegAlloc()
+
+	return leftAsm + rightAsm + ins + " " + leftReg + " " + rightReg + " " + resultReg + "\n"
+}
+
+func (this *CodeGenerator) visitUnaryExpression(node parser.Node) string {
+	if node.Type() != parser.AstTypeUnaryExpression.Name() {
+		return ""
+	}
+
+	// right
+	right := node.(parser.UnaryExpression).Value
+	rightAsm := this.visit(right)
+	utils.LogInfo("visitUnaryExpression", node.(parser.UnaryExpression).Value)
+
+	// 退出来寄存器
+	leftReg := this.Register.ReturnRegPop()
+	// 暂存结果寄存器
+	resultReg := this.Register.ReturnRegAlloc()
+
+	return rightAsm + "bool_not " + " " + leftReg + " " + resultReg + "\n"
+}
+
+func (this *CodeGenerator) visitArrayLiteral(node parser.Node) string {
+	if node.Type() != parser.AstTypeArrayLiteral.Name() {
+		return ""
+	}
+	var asm string
+	arrayValues := node.(parser.ArrayLiteral).Values
+	var vs []any
+	for _, v := range arrayValues {
+		vs = append(vs, this.visitArrayLiteralValue(v))
+	}
+	for _, v := range vs {
+		// 设置一个寄存器数值，然后 push 进去
+		asm += fmt.Sprintf("set2 %v %v\n", this.Register.ReturnRegAlloc(), v.(string))
+		asm += fmt.Sprintf("push %v\n", this.Register.ReturnRegPop())
+		this.StackOffset += 2
+	}
+	// 最后塞一个数组的数据量
+	asm += fmt.Sprintf("set2 %v %v\n", this.Register.ReturnRegAlloc(), len(arrayValues))
+	return asm
 }
 
 func (this *CodeGenerator) visitLiteral(node parser.Node) string {
@@ -244,7 +220,7 @@ func (this *CodeGenerator) visitVariableDeclaration(node parser.Node) string {
 	rightAsm := this.visit(right)
 
 	// 退出来寄存器
-	asm := rightAsm + "push2 " + this.Register.ReturnRegPop() + "\n"
+	asm := rightAsm + "push " + this.Register.ReturnRegPop() + "\n"
 	return asm
 }
 
@@ -303,4 +279,54 @@ func (this *CodeGenerator) visitIdentifier(node parser.Node) string {
 	asm = "set2 f1 " + fmt.Sprintf("%v", offset) + "\n"
 	asm += "load_from_register2 " + "f1 " + this.Register.ReturnRegAlloc() + "\n"
 	return asm
+}
+
+func (this *CodeGenerator) visitArrayLiteralValue(node parser.Node) string {
+	value := ""
+	switch node.Type() {
+	case parser.AstTypeNullLiteral.Name():
+		value = "null"
+	case parser.AstTypeStringLiteral.Name():
+		value = node.(parser.StringLiteral).Value
+	case parser.AstTypeNumberLiteral.Name():
+		value = fmt.Sprintf("%v", node.(parser.NumberLiteral).Value)
+	case parser.AstTypeBooleanLiteral.Name():
+		value = fmt.Sprintf("%v", node.(parser.BooleanLiteral).Value)
+	default:
+		utils.LogError("visitLiteral invalid type", node.Type())
+		return ""
+	}
+	return value
+}
+
+func (this *CodeGenerator) visitNullLiteral(node parser.Node) string {
+	if node.Type() != parser.AstTypeNullLiteral.Name() {
+		return ""
+	}
+
+	return "null"
+}
+
+func (this *CodeGenerator) visitNumberLiteral(node parser.Node) string {
+	if node.Type() != parser.AstTypeNumberLiteral.Name() {
+		return ""
+	}
+	value := node.(parser.NumberLiteral).Value
+	return fmt.Sprintf("%v", value)
+}
+
+func (this *CodeGenerator) visitStringLiteral(node parser.Node) string {
+	if node.Type() != parser.AstTypeStringLiteral.Name() {
+		return ""
+	}
+	value := node.(parser.StringLiteral).Value
+	return fmt.Sprintf("%v", value)
+}
+
+func (this *CodeGenerator) visitBooleanLiteral(node parser.Node) string {
+	if node.Type() != parser.AstTypeBooleanLiteral.Name() {
+		return ""
+	}
+	value := node.(parser.BooleanLiteral).Value
+	return fmt.Sprintf("%v", value)
 }
--- code_gen/codegen_test.go
+++ code_gen/codegen_test.go
@@ -14,6 +14,7 @@ func TestCodeGenerator(t *testing.T) {
 	//testCodeGenerator1_2()
 	//testCodeGenerator1_3()
 	//testCodeGenerator1_4()
+	testCodeGenerator1_5()
 }
 
 func testCodeGenerator1_1() {
@@ -146,6 +147,47 @@ func testCodeGenerator1_4() {
 	var a = 1 
 	var b = 2
 	a = 3 + b
+	`
+	fmt.Println("====================== token init =======================")
+	tokenList := lexer.TokenList()
+	tokensLexer := sansLexer.TokenList{
+		Tokens: tokenList,
+	}
+	for _, token := range tokensLexer.Tokens {
+		fmt.Printf("token %+v\n", token)
+	}
+	fmt.Println("====================== token end =======================")
+	fmt.Println("====================== parser init =======================")
+	parser := sansParser.NewSansLangParser(&tokensLexer)
+	ast := parser.Parse()
+	fmt.Printf("Ast %+v\n", ast)
+
+	// 将节点转换为JSON字符串
+	jsonData, err := json.MarshalIndent(ast, "", "    ")
+	if err != nil {
+		fmt.Println("转换为JSON时出错:", err)
+		return
+	}
+
+	// 打印JSON字符串
+	fmt.Println(string(jsonData))
+	fmt.Println("====================== parser end =======================")
+	fmt.Println("====================== asm_gen init =======================")
+
+	// 生成汇编代码
+	codeGen := NewCodeGenerator(ast)
+	codeGen.Visit()
+	fmt.Printf("%v\n", codeGen.Asm)
+
+	fmt.Println("====================== asm_gen end =======================")
+
+}
+
+func testCodeGenerator1_5() {
+	// 基本类型
+	lexer := sansLexer.SansLangLexer{}
+	lexer.Code = `
+	var a = [1,2,3,4]
 	`
 	fmt.Println("====================== token init =======================")
 	tokenList := lexer.TokenList()
